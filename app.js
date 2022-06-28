609943bd105f28b92364dcfddec4c8ca8db94b0a
Amusenna <ahmet@visiosoft.com.tr>
2022-06-28 16:12:47 +0200

    second commit before mailchimp

--- app.js
+++ app.js
@@ -7,9 +7,21 @@ const port = 3000
 
 app.use(express.static("public"));
 
+app.use(bodyParser.urlencoded({extended: true}));
+
 app.get("/", function(req, res){
   res.sendFile(__dirname + "/signup.html" )
 });
+
+app.post("/", function(req, res){
+
+  var firstName = req.body.fName;
+  var secondName = req.body.sName;
+  var email = req.body.email;
+
+  console.log(firstName, secondName, email);
+
+})
 
 app.listen (port, function(){
   console.log(`Example app listening on port ` + port);
