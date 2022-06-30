039c1f899cd94661b59f298a4b43eab8bd97e131
Amusenna <ahmet@visiosoft.com.tr>
2022-06-30 12:39:02 +0200

    Failure and success pages added

--- app.js
+++ app.js
@@ -2,6 +2,7 @@
 const bodyParser = require("body-parser");
 const express = require("express");
 const request = require("request");
+const https = require("https");
 const app = express();
 const port = 3000
 
@@ -15,13 +16,68 @@ app.get("/", function(req, res){
 
 app.post("/", function(req, res){
 
-  var firstName = req.body.fName;
-  var secondName = req.body.sName;
-  var email = req.body.email;
+  const firstName = req.body.fName;
+  const secondName = req.body.sName;
+  const email = req.body.email;
 
-  console.log(firstName, secondName, email);
+  const data = {
+
+     members: [
+       {
+         email_address: email,
+         status: "subscribed",
+         merge_fields: {
+           FNAME: firstName,
+           LNAME: secondName
+         }
+       }
+     ]
+  };
+
+
+  const jsonData = JSON.stringify(data);
+
+  const url = "https://us12.api.mailchimp.com/3.0/lists/b921c20e94"
+
+  const options = {
+              method: "POST",
+              auth: "ahmet1:a739e8f6bfd015c099d38ae41711539ae-us12"
+
+  }
+
+  const request = https.request(url, options, function(response) {
+
+    if (response.statusCode === 200) {
+      res.sendFile(__dirname + "/success.html");
+    } else {
+      res.sendFile(__dirname + "/failure.html")
+    }
+
+
+    response.on("data", function(data){
+      console.log(JSON.parse(data));
+    })
+  })
+
+  request.write(jsonData);
+  request.end();
+
 
 })
+
+app.post("/failure", function(req, res) {
+  res.redirect("/");
+});
+
+
+
+
+// API KEY
+// 739e8f6bfd015c099d38ae41711539ae-us12
+
+// Audience ID or List ID
+// b921c20e94
+
 
 app.listen (port, function(){
   console.log(`Example app listening on port ` + port);
